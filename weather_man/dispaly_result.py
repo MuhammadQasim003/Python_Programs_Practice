5d0c40faebbc97700acec3f3c3c3264f9a81b4a2
Muhammad Qasim <138574285+MuhammadQasim003@users.noreply.github.com>
2023-07-21 17:43:58 +0500

    Update dispaly_result.py

--- weather_man/dispaly_result.py
+++ weather_man/dispaly_result.py
@@ -1,5 +1,18 @@
 def print_year_result(max_temprature,max_temperature_month,max_temperature_day,min_temprature,min_temperature_month,min_temperature_day,max_humidity,max_humidity_month,max_humidity_day):
-    ''' this function will display the result of year '''
+    """
+        This function will display the result of year
+    Args:
+        max_temprature (str): variable contain the value of max temprature 
+        max_temperature_month (str): variable contain month of the max temprature
+        max_temperature_day (str): varable contian day of the max temprature
+        min_temprature (str): variable contain the value of min temprature
+        min_temperature_month (str): varaible contain month of the min temprature
+        min_temperature_day (str):  variable contain day of the min temprature
+        max_humidity (str): variable contain the value max humidity
+        max_humidity_month (str): variable contain month of the max humidity
+        max_humidity_day (str): variable contain day of the max humidity
+    """
+    
     print("Highest: {}C on {} {}".format(int(max_temprature), max_temperature_month, max_temperature_day))
     print("Lowest: {}C on {} {}".format(int(min_temprature), min_temperature_month, min_temperature_day))
     print("Humidity: {}% On {} {}".format(int(max_humidity), max_humidity_month, max_humidity_day))
