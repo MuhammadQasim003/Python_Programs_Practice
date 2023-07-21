c81d44a3f3388d97ae3875ea753292ce4f701a5f
Muhammad Qasim <138574285+MuhammadQasim003@users.noreply.github.com>
2023-07-21 17:45:18 +0500

    Update date_time_convertion.py

--- weather_man/date_time_convertion.py
+++ weather_man/date_time_convertion.py
@@ -1,7 +1,16 @@
 from datetime import datetime 
 
 def convert_date_to_month_format(x):
-    ''' this function will convert date from 2000-1-1 to 1 Jan 2000 format'''
+    """
+     this function will convert date from 2000-1-1 to 1 Jan 2000 format
+    Args:
+        x (int): contian the value of date in 2000-1-1 format
+
+    Returns:
+
+        list: list contain the month and dat required for further processing
+    """
+    
     date_str = str(x)
     date_obj = datetime.strptime(date_str, '%Y-%m-%d')
     month = date_obj.strftime('%B')
